34716b0c759ab05a485468345cb5470d3411b228
mion00 <mion00@gmail.com>
2015-12-15 17:17:07 +0100

    Sprint 1

--- js/app.js
+++ js/app.js
@@ -4,28 +4,103 @@
  */
 
 var SantaModel = {
-  
-  /* Initializes the model with a list of requests, 
-   * and sets the first one as the current one 
-   */
-   init : function(list){
-   },
-  
-   /* It moves "current" to the next request */
-   next : function (){
-   },
-  
-   /* Returns the current request. 
-    * If all requests have been processed (there is no current one), it returns null 
-    */
-   getCurrentRequest : function () {
-   },  
-    
-   /* Packs the given item if it fulfills the current request.       
-    * returns 1 if the given item fulfills the request (= answer)
-    * returns 0 if the given item does not fulfill the request
-    */
-   pack : function(item) {
-   }      
-  
+
+    /* Initializes the model with a list of requests,
+     * and sets the first one as the current one
+     */
+    init: function (list) {
+        this.packs = [];
+        this.requests = requests;
+        this.current = 0;
+    },
+
+    /* It moves "current" to the next request */
+    next: function () {
+        this.current += 1;
+    },
+
+    /* Returns the current request.
+     * If all requests have been processed (there is no current one), it returns null
+     */
+    getCurrentRequest: function () {
+        if (this.current == this.requests.length) {
+            return null;
+        } else {
+            return this.requests[this.current];
+        }
+    },
+
+    /* Packs the given item if it fulfills the current request.
+     * returns 1 if the given item fulfills the request (= answer)
+     * returns 0 if the given item does not fulfill the request
+     */
+    pack: function (item) {
+        var request = SantaModel.getCurrentRequest();
+        SantaModel.next();
+        if (request != null) {
+            if (request.answer == item) {
+                return 1;
+            } else {
+                return 0;
+            }
+
+        }
+    }
+
 };
+
+var SantaView = {
+    init: function() {
+        this.bindAnswers();
+    },
+    showRequest: function (request) {
+        $(".question").html(request.question);
+        $(".question-items").empty();
+        request.options.forEach(function (elem) {
+            $(".question-items").append("<li>" + elem + "</li>");
+        });
+    },
+    showResult: function (points) {
+        $(".question").hide();
+        $(".question-items").hide();
+        $(".result").html("Total points: " + points);
+    },
+    bindAnswers: function() {
+        $(".question-items").click(SantaController.clickHandler);
+    }
+};
+
+var SantaController = {
+    init: function() {
+        this.points = 0;
+        SantaModel.init();
+        SantaView.init();
+        this.showNextRequest();
+    },
+    showNextRequest: function() {
+        var request = SantaModel.getCurrentRequest();
+        if (request != null) {
+            SantaView.showRequest(request);
+        } else {
+            SantaView.showResult(this.points);
+        }
+    },
+    clickHandler: function(event) {
+        var li = $(event.target).filter("li");
+        if(li.length > 0) {
+            var value = li.html();
+            SantaController.checkAnswer(value);
+        }
+    },
+    checkAnswer: function (answer) {
+        var pack = SantaModel.pack(answer);
+        if (pack > 0) {
+            this.points += 1;
+        }
+        SantaController.showNextRequest();
+    }
+};
+
+$(document).ready(function() {
+   SantaController.init();
+});
--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -1,10 +1,30 @@
 /* Remember that blanket will only work with brackets live preview */
 /* Try to maximise the coverage of the SantaModel object */
 
-describe("Santa", function() {
+describe("Santa", function () {
 
-  it("should...", function() {  
-    
-  });    
- 
+    it("should init the data", function () {
+        SantaModel.init();
+        expect(SantaModel.current).toBe(0);
+        expect(SantaModel.requests.length).toBe(requests.length);
+    });
+
+    it("should show the result at the end", function () {
+        SantaController.init();
+        SantaModel.current = requests.length;
+        spyOn(SantaView, "showResult");
+        SantaController.showNextRequest();
+        expect(SantaView.showResult).toHaveBeenCalled();
+    });
+
+    it("check the correct answer", function () {
+        SantaController.init();
+        expect(SantaModel.pack("yes")).toBe(0);
+        expect(SantaModel.pack("pippo")).toBe(0);
+        expect(SantaModel.pack("no")).toBe(1);
+    });
+
+    it("..", function () {
+
+    });
 });
